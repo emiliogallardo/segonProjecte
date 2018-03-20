6f7057124ec2c2bee0c59831b490a819da9e21cb
Emilio <emiliogallardo@iespaucasesnoves.cat>
2018-03-20 20:24:20 +0100

    commit8

--- src/primerProjecte/laMevaClasse.java
+++ src/primerProjecte/laMevaClasse.java
@@ -1,13 +1,8 @@
 package primerProjecte;
 
 public class laMevaClasse {
-	int a;
-<<<<<<< Upstream, based on master
 	int bien;
 	int c;
-=======
-	int b;
-	int cabra;
->>>>>>> 93d35d3 commit6
+
 
 }
